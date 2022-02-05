a05aad9c46da18628862a3b8bde78e58f25d5898
muramasa927 <snight16@icloud.com>
2022-02-05 13:37:56 +0900

    remove recoverable column
    パスワードリセット関連のカラムを削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,6 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions',
     :confirmations => 'users/confirmations'
   } 
-  
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
 
   #開発環境のみletter_openerのURLを追加
   if Rails.env.development?
