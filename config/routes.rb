546aea58249c78dc0e10156c2aad4eea286098c7
muramasa927 <snight16@icloud.com>
2022-02-06 15:24:58 +0900

    change views/passwords/new

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
-    :confirmations => 'users/confirmations'
+    :confirmations => 'users/confirmations',
+    :passwords => 'users/passwords'
   } 
 
   #開発環境のみletter_openerのURLを追加
