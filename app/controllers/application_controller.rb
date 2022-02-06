d9e960a6a89e45e0fc3364bd4065b27b94dddf9a
muramasa927 <snight16@icloud.com>
2022-02-06 16:35:21 +0900

    add users controller
    usersコントローラーを追加　ログイン後dash_boradsへ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-    dash_poards_users_path
+    dash_boards_users_path(resource)
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+	def dash_boards
+
+	end
+end
