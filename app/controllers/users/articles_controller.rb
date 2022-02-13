c18655a6afc8e4eaf62f4e51a160b86e2c424525
muramasa927 <snight16@icloud.com>
2022-02-13 15:23:51 +0900

    add article/edit
    articleのedit,update,destroy機能を追加

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -1,6 +1,8 @@
 class Users::ArticlesController < ApplicationController
 	#before_action :configure_sign_up_params, only: [:create]
 	def index
+		@user = User.find(current_user.id)
+		@articles = @user.articles.all
 	end
 
 	def show
@@ -11,12 +13,33 @@ class Users::ArticlesController < ApplicationController
 	end
 
 	def create
-		
+		@user = User.find(current_user.id)
+		@article = @user.articles.new(article_params)
+		@article.save!
 		redirect_to users_articles_path
 	end
 
+	def edit
+		@user = User.find(current_user.id)
+		@article = @user.articles.find(params[:id])
+	end
+
+	def update
+		@user = User.find(current_user.id)
+		@article = @user.articles.find(params[:id])
+		@article.update!(article_params)
+		redirect_to users_articles_path
+	end
+	
+  def destroy
+    Article.find(params[:id]).destroy
+    flash[:success] = "記事を削除しました"
+    redirect_to users_articles_url
+  end
+	
+
 private
 	def article_params
-		params.require(:post).permit(:title, :content, :image)
+		params.require(:article).permit(:title, :content, :image, :remove_image)
 	end
 end
